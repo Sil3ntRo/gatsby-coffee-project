9339f09fb45751a5a42d1dfb64fef39066b6e99d
Rohan Gayle <rgayle33614@gmail.com>
2019-06-15 18:36:15 -0400

    Added About Me page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,34 +2,53 @@ import React from "react";
 import { Link, graphql } from "gatsby";
 
 import Layout from "../components/layout";
-
 import SEO from "../components/seo";
-import BackgroundSec from '../components/Globals/BackgroundSec';
+import BackgroundSection from "../components/Globals/BackgroundSec";
 import Info from "../components/Home/Info";
+import Menu from "../components/Home/Menu";
+import Contact from "../components/Home/Contact";
 
-
-const IndexPage = ({data}) => (
+const IndexPage = ({ data }) => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]}/>
-    <BackgroundSec img={data.img.childImageSharp.fluid}
-                   title="regular joe's"
-                   styleClass="default-background" />
-  <Info/>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="regular joe's"
+    />
+    <Info />
+    <Menu items={data.menu} />
+    <Contact/>
   </Layout>
-)
+);
 
 export const query = graphql`
   {
-    img:file(relativePath: {eq:
-    "default-background.jpeg"}){
-      childImageSharp{
-        fluid{
+    img: file(relativePath: { eq: "default-background.jpeg" }) {
+      childImageSharp {
+        fluid {
           ...GatsbyImageSharpFluid_tracedSVG
         }
       }
     }
+    menu: allContentfulCoffeeItem {
+      edges {
+        node {
+          id
+          title
+          description {
+            description
+          }
+          price
+          category
+          image {
+            fixed(width: 50, height: 50) {
+              ...GatsbyContentfulFixed_tracedSVG
+            }
+          }
+        }
+      }
+    }
   }
-`
+`;
 
-
-export default IndexPage
+export default IndexPage;
